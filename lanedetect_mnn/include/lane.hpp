7bce593df0d852ba829134015bccac9aaa572ba9
fujing tian <tianfujing@foxmail.com>
2022-06-08 11:02:33 +0800

    hhh

---
+++ lanedetect_mnn/include/lane.hpp
@@ -0,0 +1,58 @@
+#ifndef __LANE_H__
+#define __LANE_H__
+
+#include <iostream>
+#include <cmath>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include "MNN/Interpreter.hpp"
+#include "MNN/ImageProcess.hpp"
+
+class LaneDetect
+{
+    typedef struct Lanes
+    {
+        float x1;  // 起点
+        float y1;
+        float x2;  // 终点
+        float y2;
+        float lens;
+        float conf;
+
+    }Lanes;
+    
+
+    public:
+        LaneDetect(const char* modelPath);
+        LaneDetect();
+        ~LaneDetect();
+        // 数据预处理，放入tensor中
+        void processImg(const cv::Mat& img);
+        // 推理，输出结果指针
+        const float* inference(const cv::Mat& img);
+        std::vector<Lanes> decodeHeatmap(const float* heatmap);
+        void showImg(const cv::Mat& img,std::vector<Lanes> Lanes);
+        inline int clip(float value);
+    private:
+        std::shared_ptr<MNN::Interpreter> m_net = nullptr;
+        MNN::ScheduleConfig m_config;
+        MNN::Session *m_session;
+        MNN::Tensor *m_inTensor = nullptr;
+        MNN::BackendConfig m_backend_config;
+
+        std::vector<Lanes> m_lanes;
+        const char* m_model_path = "../models/mlsd_with_max_sigmoid.mnn";
+        float m_score_thresh = 0.2; // 阈值
+        int m_input_size     = 512; // 输入尺寸
+        int m_hm_size  = 256; // 特征图大小
+        int m_min_len  = 20; // 预测线段的最短长度
+        int m_top_k    = 200; // 取200条线
+        int m_precision  = 0; // 精度
+        int m_power      = 0; // 能耗
+        int m_memory     = 0; // 内存
+
+};
+
+
+#endif
---
+++ lanedetect_mnn/src/lane.cpp
@@ -0,0 +1,155 @@
+#include "lane.hpp"
+
+LaneDetect::LaneDetect()
+{
+    m_net = std::shared_ptr<MNN::Interpreter>(MNN::Interpreter::createFromFile(m_model_path));
+    m_backend_config.precision = (MNN::BackendConfig::PrecisionMode) m_precision; // 精度
+    m_backend_config.power = (MNN::BackendConfig::PowerMode) m_power; // 功耗
+    m_backend_config.memory = (MNN::BackendConfig::MemoryMode) m_memory; // 内存占用
+	m_config.backendConfig = &m_backend_config;
+	m_config.type = MNN_FORWARD_AUTO;
+    m_config.numThread = 4;
+	m_session = m_net->createSession(m_config); //创建session
+    m_inTensor = m_net->getSessionInput(m_session, NULL);
+	std::cout << "session created" << std::endl;
+}
+
+LaneDetect::LaneDetect(const char* model_path)
+{
+    m_net = std::shared_ptr<MNN::Interpreter>(MNN::Interpreter::createFromFile(model_path));
+    m_backend_config.precision = (MNN::BackendConfig::PrecisionMode) m_precision; // 精度
+    m_backend_config.power = (MNN::BackendConfig::PowerMode) m_power; // 功耗
+    m_backend_config.memory = (MNN::BackendConfig::MemoryMode) m_memory; // 内存占用   
+	m_config.backendConfig = &m_backend_config;
+	m_config.type = MNN_FORWARD_AUTO;
+    m_config.numThread = 4;
+	m_session = m_net->createSession(m_config); //创建session
+    m_inTensor = m_net->getSessionInput(m_session, NULL);
+	std::cout << "session created" << std::endl;
+}
+
+LaneDetect::~LaneDetect()
+{
+    m_net->releaseModel();
+    m_net->releaseSession(m_session);
+    m_lanes.clear();
+}
+
+inline int LaneDetect::clip(float value)
+{
+    if (value > 0 && value < m_input_size)
+        return int(value);
+    else if (value < 0)
+        return 1;
+    else
+        return m_input_size - 1;
+
+}
+
+void LaneDetect::showImg(const cv::Mat& img,std::vector<LaneDetect::Lanes> Lanes)
+{
+    cv::Mat tmp_img = img.clone();
+    int h = tmp_img.rows;
+    int w = tmp_img.cols;
+    
+    for(auto line:Lanes)
+    {
+        float x1 = line.x1;
+        float y1 = line.y1;
+        float x2 = line.x2;
+        float y2 = line.y2;
+        x1 = clip(w * x1 / (m_input_size / 2));
+        x2 = clip(w * x2 / (m_input_size / 2));
+        y1 = clip(h * y1 / (m_input_size / 2));
+        y2 = clip(h * y2 / (m_input_size / 2));
+
+        cv::line(tmp_img,cv::Point(x1,y1),cv::Point(x2,y2), cv::Scalar(0, 0, 255));
+    }
+
+    cv::imshow("img",tmp_img);
+    cv::waitKey(0);
+    return ;
+}
+
+
+
+std::vector<LaneDetect::Lanes> LaneDetect::decodeHeatmap(const float* hm)
+{   
+    // 线段中心点(256*256),线段偏移(4*256*256)
+    const float*  displacement = hm+m_hm_size*m_hm_size;
+    // exp(center,center);
+    std::vector<float> center;
+    for (int i = 0;i < m_hm_size*m_hm_size; i++)
+    {
+        center.push_back( hm[i] ); // mlsd.mnn原始需要1/(exp(-hm[i]) + 1)
+    }
+    center.resize(m_hm_size*m_hm_size);
+
+    std::vector<int> index(center.size(), 0);
+    for (int i = 0 ; i != index.size() ; i++) {
+        index[i] = i;
+    }
+    sort(index.begin(), index.end(),
+        [&](const int& a, const int& b) {
+            return (center[a] > center[b]); // 从大到小排序
+        }
+    );
+    
+    for (int i = 0; i < index.size(); i++)
+    {
+        int yy = index[i]/m_hm_size; // 除以宽得行号
+        int xx = index[i]%m_hm_size; // 取余宽得列号
+        Lanes Lane;
+        Lane.x1 = xx + displacement[index[i] + 0*m_hm_size*m_hm_size];
+        Lane.y1 = yy + displacement[index[i] + 1*m_hm_size*m_hm_size];
+        Lane.x2 = xx + displacement[index[i] + 2*m_hm_size*m_hm_size];
+        Lane.y2 = yy + displacement[index[i] + 3*m_hm_size*m_hm_size];
+        Lane.lens = sqrt(pow(Lane.x1 - Lane.x2,2) + pow(Lane.y1 - Lane.y2,2));
+        Lane.conf = center[index[i]];
+
+        if (center[index[i]] > m_score_thresh && m_lanes.size() < m_top_k)
+        {
+            if ( Lane.lens > m_min_len)
+                m_lanes.push_back(Lane);
+        }
+        else
+            break;
+    }
+    
+    return m_lanes;
+
+}
+
+
+void LaneDetect::processImg(const cv::Mat& img)
+{   
+    cv::Mat preImage = img.clone();
+    cv::cvtColor(preImage,preImage,cv::COLOR_BGR2RGB);
+    cv::resize(preImage,preImage,cv::Size(m_input_size,m_input_size));
+	preImage.convertTo(preImage,CV_32FC3,1/127.5,-1); // (img / 127.5) - 1.0
+    std::vector<cv::Mat> bgrChannels(3);
+    cv::split(preImage, bgrChannels);
+    std::vector<float> chwImage;
+    for (auto i = 0; i < bgrChannels.size(); i++)
+    {  
+        //HWC->CHW
+        std::vector<float> data = std::vector<float>(bgrChannels[i].reshape(1, preImage.cols * preImage.rows));
+        chwImage.insert(chwImage.end(), data.begin(), data.end());
+    }
+    
+	auto nchw_Tensor = new MNN::Tensor(m_inTensor, MNN::Tensor::CAFFE); // tensorflow: nhwc, caffe: nchw, caffe_c4: nc4hw4
+    ::memcpy(nchw_Tensor->host<float>(), chwImage.data(), nchw_Tensor->elementSize() * 4);
+    m_inTensor->copyFromHostTensor(nchw_Tensor);
+
+}
+
+const float* LaneDetect::inference(const cv::Mat& img)
+{
+    processImg(img);
+    m_net->runSession(m_session);
+    auto output= m_net->getSessionOutput(m_session, NULL);
+    auto nchwTensor = new MNN::Tensor(output, MNN::Tensor::CAFFE);
+    output->copyToHostTensor(nchwTensor);
+    auto score = nchwTensor->host<float>(); // 得到结果指针
+    return score;
+}
